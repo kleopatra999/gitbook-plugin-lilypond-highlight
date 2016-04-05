cd3346eddc7be65de8d90e1185f92d7b37f1ef8d
Urs Liska <git@ursliska.de>
2016-04-06 00:29:59 +0200

    Some updates to index.js

--- index.js
+++ index.js
@@ -6,8 +6,10 @@
    This means python-ly has to be installed through pip
    (or from https://github.com/wbsoft/python-ly), and the standalone script `ly`
    has to be accessible.
-   Depending on the context it *may* be possible to run Gitbook in a context
-   where the PYTHONPATH environment variable has been set.
+   Depending on the context it *may* be necessary to run Gitbook in a context
+   where the PYTHONPATH environment variable has been explicitly set. For example
+   this may happen in a Git hook on the server (where no environment is set up
+   in a non-login shell).
    */
 'use strict';
 
@@ -22,14 +24,18 @@ module.exports = {
                 // Prepare command
                 var cmd = "ly";
                 var options = [];
+
                 // TODO: conditionally insert options for "code"
+                if (block.args.length > 0) {
+                    console.log ("Have an arg: " + block.args[0]);
+                };
                 options.push("highlight");
 
                 // Execute command, passing the content of the block through stdin
                 var run_cl = spawn(cmd, [options], { input: block.body });
 
                 // Handle result
-                if (run_cl.stderr != null) {
+                if (run_cl.stderr != "") {
                     console.log(run_cl.stderr.toString())
                 };
                 if (run_cl.stdout != null) {
