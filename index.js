48e488c39b7524df698bb02d175f35853c5f248f
Urs Liska <git@ursliska.de>
2016-04-05 16:18:41 +0200

    Initial working version (bump to 0.5.0)

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
-// Main file for the lilypond-styles gitbook plugin
+/* gitbook-plugin-lilypond-highlight
+   This plugin only exposes one item: {% lilypond %}, which takes input in the
+   GNU LilyPond language and produces syntax highlighting for it.
+
+   There is one external dependency: python-ly
+   This means python-ly has to be installed through pip
+   (or from https://github.com/wbsoft/python-ly), and the standalone script `ly`
+   has to be accessible.
+   Depending on the context it *may* be possible to run Gitbook in a context
+   where the PYTHONPATH environment variable has been set.
+   */
 'use strict';
 
 module.exports = {
-  blocks: {
-      lilypond: {
-        process: function(block) {
-          // Call python-ly to colorize the LilyPond input
-          // Return a <pre class="lilypond"> block
+    blocks: {
+        lilypond: {
+            process: function(block) {
 
-          var sys = require('sys')
+                var sys = require('sys')
+                var spawn = require('child_process').spawnSync;
 
-          var spawn = require('child_process').spawnSync;
+                // Prepare command
+                var cmd = "ly";
+                var options = [];
+                // TODO: conditionally insert options for "code"
+                options.push("highlight");
 
-          // Call the Python script (which uses python-ly)
-          var cmd = "python/colorize-lilypond";
-          var run_cl = spawn(cmd, [block.body]);
+                // Execute command, passing the content of the block through stdin
+                var run_cl = spawn(cmd, [options], { input: block.body });
 
-          if (run_cl.stderr != null) {
-            console.log(run_cl.stderr.toString())
-          };
-          if (run_cl.stdout != null) {
-            return run_cl.stdout.toString();
-          }
-          else {
-            return "<p><strong>LilyPond highlighting returned no result</strong></p>"
-          };
+                // Handle result
+                if (run_cl.stderr != null) {
+                    console.log(run_cl.stderr.toString())
+                };
+                if (run_cl.stdout != null) {
+                    return run_cl.stdout.toString();
+                }
+                else {
+                    return "<p><strong>LilyPond highlighting returned no result</strong></p>"
+                };
+            }
         }
-      }
-  }
+    }
 };
